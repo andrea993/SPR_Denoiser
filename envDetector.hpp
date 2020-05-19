1b0d388c046d3bfcdce9e241dda56c760a79a94c
Dario Fagotto <dario.fagotto@gmail.com>
2020-05-19 18:29:57 +0200

    Added git ignore and local copy of eigen

--- envDetector.hpp
+++ envDetector.hpp
@@ -5,10 +5,10 @@ class EnvDetector
         EnvDetector(double k1, double k2, double Ts) : k1(k1), k2(k2), x1(.0), x2(.0), Ts(Ts) {}
 
         double GetSmp(double u) {
-            auto y{ (x1 > x2) ? x1 : x2 };
-            auto ϵ{ u - y };
-            x1 += k1*Ts*ϵ;
-            x2 += k2*Ts*ϵ;
+            auto y { (x1 > x2) ? x1 : x2 };
+            auto epsilon { u - y };
+            x1 += k1*Ts*epsilon;
+            x2 += k2*Ts*epsilon;
             
             return y;
         }
