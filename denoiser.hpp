741ceae3635833b134014b6dea6f801d997cebac
Andrea <andrea69x@hotmail.it>
2020-07-29 13:44:26 +0200

    discretization

--- denoiser.hpp
+++ denoiser.hpp
@@ -1,18 +1,17 @@
 #include <eigen3/Eigen/Dense>
+#include <eigen3/Eigen/Eigenvalues>
 
 #include "envDetector.hpp"
 #include "kalmanFilter.hpp"
 
-//#include <iostream>
-
-
 class Denoiser {
     public:
         const double Ts;
-
-        Denoiser(double Ts = 0.01) : Ts(Ts), ed{EnvDetector(k1_ed, k2_ed, Ts), 
-                                                EnvDetector(k1_ed, k2_ed, Ts),
-                                                EnvDetector(k1_ed, k2_ed, Ts)}
+        
+        Denoiser(double Ts = 0.01) : Ts(Ts), kf(StepInvariant(ss, Ts), Q), ed{EnvDetector(k1_ed, k2_ed, Ts), 
+                                                                            EnvDetector(k1_ed, k2_ed, Ts),
+                                                                            EnvDetector(k1_ed, k2_ed, Ts)}
+                                                                            
         {}
 
         Eigen::Vector2d GetSmp(Eigen::Vector2d u) {
@@ -23,7 +22,7 @@ class Denoiser {
             auto err_pow{ (kf_in - kf_out.Yhat).array().pow(2) };
             R.diagonal() << ed[0].GetSmp(err_pow(0)), ed[1].GetSmp(err_pow(1)), ed[2].GetSmp(err_pow(2));
 
-
+            
             //std::cout << kf_out.Xhat.transpose() << ' ' << R.diagonal().transpose() << '\n';
 
             return kf_out.Xhat;
@@ -33,6 +32,23 @@ class Denoiser {
         const double k1_ed = .99;
         const double k2_ed = .8;
         const double k_R = 100.0;
+        const double tauC = .005;
+        const double EPS = 1e-3;
+
+        static StateSpace StepInvariant (const StateSpace &ss, double Ts) { 
+            auto ssd{ ss };
+            //Eigen::SelfAdjointEigenSolver<A_t> eigensolver(ss.A*Ts);
+            Eigen::EigenSolver<A_t> es(ss.A*Ts);
+
+            auto eigvals{ es.eigenvalues().real()  };
+            auto eigvecs{ es.eigenvectors().real() };
+            
+            A_t eigvmatd{ eigvals.array().exp().matrix().asDiagonal() };
+
+            ssd.A = (eigvecs*eigvmatd*eigvecs.inverse());         
+        
+            return ssd;
+        } 
 
         const R_t R0{ (R_t() << 10,   0,   0,
 								0,  10,   0,
@@ -44,13 +60,13 @@ class Denoiser {
         EnvDetector ed[3];
 
         StateSpace ss {
-            (A_t() <<   0.135335283236613,   0.864659040108588, 
-                        0,                   0.999990000050000).finished(),
+            (A_t() <<  -1/tauC,   1/tauC, 
+                        0,        -EPS).finished(),
             B_t(), 
             (C_t() <<   1,   0, 
                         1,   0,
                         1,  -1).finished()
         };
-        KalmanFilter kf{ ss, Q };
+        KalmanFilter kf;
 
 };
